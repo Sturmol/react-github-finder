56828f40999e7fd6ba6d63cf1e183fc0393b81c2
Miguel Guelbenzu <miguel.guelbenzu@aperto.com>
2019-07-24 16:54:38 +0200

    Add Users component and render list of users

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
-
     render() {
-      const name = "john doe";
-      const loading = true;
-      const showName = true;
-
-      return (
-          <div>
-              <h1> Hello from react</h1>
-              {loading ? <h4>loading...</h4> :  <p>{ name.toUpperCase()}</p>}
-          </div>
-      )
+        return (
+            <div className="App">
+                <Navbar />
+                <UserItem />
+            </div>
+        )
     }
 }
 
