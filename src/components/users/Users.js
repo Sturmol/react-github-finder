720f3491dd4d1467b80837c7ba5debf36b04e98d
Miguel Guelbenzu <miguel.guelbenzu@aperto.com>
2019-07-25 12:39:57 +0200

    Fetch data from api and refactor components

---
+++ src/components/users/Users.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class Users extends Component {
+    state = {
+        users: [
+            {
+                id: '1',
+                login: 'mojombo',
+                avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+                html_url: 'https://github.com/mojombo'
+            },
+            {
+                id: '2',
+                login: 'errfree',
+                avatar_url: 'https://avatars2.githubusercontent.com/u/44?v=4',
+                html_url: 'https://github.com/errfree'
+            },
+            {
+                id: '3',
+                login: 'jamesgolick',
+                avatar_url: 'https://avatars2.githubusercontent.com/u/37?v=4',
+                html_url: 'https://github.com/jamesgolick'
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div style={userStyle}>
+                {this.state.users.map( user => <UserItem key={user.id} user={user}/>)}
+            </div>
+        )
+    }
+}
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+export default Users
